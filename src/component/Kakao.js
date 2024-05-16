c7dde5dc62dd9ab62b1a0fe5536c2f6fe18d5983
K0seoyoung <koseo12@hufs.ac.kr>
2024-05-17 02:05:25 +0900

    feat : 좌표 표시

--- src/component/Kakao.js
+++ src/component/Kakao.js
@@ -29,7 +29,7 @@ function Kakao() {
                 level: 3
             };
             const map = new window.kakao.maps.Map(container, options);
-            
+
             // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
             var zoomControl = new window.kakao.maps.ZoomControl();
             map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
@@ -38,7 +38,7 @@ function Kakao() {
 
             markerdata.forEach((data) => {
                 var position = new window.kakao.maps.LatLng(data.lat, data.lng);
-                var content = `<div class="wrap"> 
+                var contentHTML = `<div class="wrap"> 
                                 <div class="info"> 
                                     <div class="title"> 
                                         ${data.name}
@@ -49,10 +49,25 @@ function Kakao() {
                                             <div class="jibun ellipsis">${data.name}는</div> 
                                             <div class="jibun ellipsis">${data.operatorName}회사에서 만든 충전기이다.</div> 
                                         </div> 
-                                        <button class="img"></button> 
+                                        <button class="img" id="bookmarkButton"></button> 
                                     </div> 
                                 </div> 
                             </div>`;
+                // //content HTMLElement 생성
+                // var content = document.createElement('div');
+                // content.innerHTML = contentHTML;
+                // //event 추가
+                // var bookmarkBtn = document.createElement('button');
+                // bookmarkBtn.appendChild(document.createTextNode(''));
+                // bookmarkBtn.onclick = function () {
+                //     var currentImage = bookmarkBtn.style.backgroundImage;
+                //     if (currentImage.includes('bookmark_clicked.svg')) {
+                //         bookmarkBtn.style.backgroundImage = 'url(./SVG/bookmark.svg)';
+                //     } else {
+                //         bookmarkBtn.style.backgroundImage = 'url(./SVG/bookmark_clicked.svg)';
+                //     }
+                // }
+                // content.appendChild(bookmarkBtn);
 
                 // 마커를 표시할 지도
                 var marker = new window.kakao.maps.Marker({
@@ -62,7 +77,7 @@ function Kakao() {
                 // customize overlay
                 var overlay = new window.kakao.maps.CustomOverlay({
                     position: position,
-                    content: content,
+                    content: contentHTML,
                     yAnchor: 1
                 });
 
@@ -88,6 +103,14 @@ function Kakao() {
             var bounds = new window.kakao.maps.LatLngBounds();    
             points.forEach(point => bounds.extend(point));
             map.setBounds(bounds);
+
+            window.kakao.maps.event.addListener(map,'bounds_changed', function() {
+                var bound = map.getBounds();
+                var swLatlng = bound.getSouthWest();
+                var neLatlng = bound.getNorthEast();
+                console.log('<p>남서쪽 위경도 : ' + swLatlng.toString() + ',' + 
+                '북동쪽 위경도 : '+ neLatlng.toString());
+            })
         }
     };
 
