8d0c9df29749e19aae1bf3462ff998936b2bca4c
Hongjunior <seokjun09@gmail.com>
2024-05-17 15:25:22 +0900

    검색창 예시코드 구현

--- src/component/Kakao.js
+++ src/component/Kakao.js
@@ -1,80 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { markerdata } from "./Data/markerData";
 import './Kakao.css'
 
 function Kakao() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAOMAP_API_KEY}&libraries=services,drawing`;
-        script.async = true;
-        script.onload = () => {
-            mapscript();
-        };
-        script.onerror = (error) => {
-            console.error("Error loading Kakao maps script:", error);
-        };
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
-
-    const mapscript = () => {
+    const mapscript = useCallback(() => {
         //지도 표시
         if (window.kakao && window.kakao.maps) {
             var container = document.getElementById('map');
             var options = {
                 center: new window.kakao.maps.LatLng(37.336382765356035, 127.26531823399158),
-                level: 3,
-                MapTypeId : window.kakao.maps.MapTypeId.ROADMAP //지도 종류
+                level: 3
             };
             const map = new window.kakao.maps.Map(container, options);
-
-            map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC); //실시간 교통
-            // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.ROADVIEW); //로드뷰
-            var mapTypeControl = new window.kakao.maps.MapTypeControl(); //type controller
-            map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.RIGHT);
-
-
-            // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-            var zoomControl = new window.kakao.maps.ZoomControl();
-            map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-
+            
             var overlays = [];
 
             markerdata.forEach((data) => {
                 var position = new window.kakao.maps.LatLng(data.lat, data.lng);
-                var contentHTML = `<div class="wrap"> 
+                var content = `<div class="wrap"> 
                                 <div class="info"> 
                                     <div class="title"> 
-                                        ${data.name}
-                                    </div>
+                                        ${data.name}  
+                                    </div> 
                                     <div class="body"> 
+                                        <div class="img"> 
+                                            <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70"> 
+                                        </div> 
                                         <div class="desc"> 
                                             <div class="ellipsis">${data.address}</div> 
                                             <div class="jibun ellipsis">${data.name}는</div> 
                                             <div class="jibun ellipsis">${data.operatorName}회사에서 만든 충전기이다.</div> 
                                         </div> 
-                                        <button class="img" id="bookmarkButton"></button> 
                                     </div> 
                                 </div> 
                             </div>`;
-                // //content HTMLElement 생성
-                // var content = document.createElement('div');
-                // content.innerHTML = contentHTML;
-                // //event 추가
-                // var bookmarkBtn = document.createElement('button');
-                // bookmarkBtn.appendChild(document.createTextNode(''));
-                // bookmarkBtn.onclick = function () {
-                //     var currentImage = bookmarkBtn.style.backgroundImage;
-                //     if (currentImage.includes('bookmark_clicked.svg')) {
-                //         bookmarkBtn.style.backgroundImage = 'url(./SVG/bookmark.svg)';
-                //     } else {
-                //         bookmarkBtn.style.backgroundImage = 'url(./SVG/bookmark_clicked.svg)';
-                //     }
-                // }
-                // content.appendChild(bookmarkBtn);
 
                 // 마커를 표시할 지도
                 var marker = new window.kakao.maps.Marker({
@@ -84,7 +43,7 @@ function Kakao() {
                 // customize overlay
                 var overlay = new window.kakao.maps.CustomOverlay({
                     position: position,
-                    content: contentHTML,
+                    content: content,
                     yAnchor: 1
                 });
 
@@ -100,29 +59,40 @@ function Kakao() {
             window.kakao.maps.event.addListener(map, "click", function () {
                 closeOverlays();
             });
-            function closeOverlays() {
-                overlays.forEach(function(overlay) {
-                    overlay.setMap(null); 
-                });
-            }
-            //지도 범위 재설정
-            var points = markerdata.map(data => new window.kakao.maps.LatLng(data.lat, data.lng));
-            var bounds = new window.kakao.maps.LatLngBounds();    
-            points.forEach(point => bounds.extend(point));
-            map.setBounds(bounds);
+        }
+    }, []);
 
-            window.kakao.maps.event.addListener(map,'bounds_changed', function() {
-                var bound = map.getBounds();
-                var swLatlng = bound.getSouthWest();
-                var neLatlng = bound.getNorthEast();
-                console.log('<p>남서쪽 위경도 : ' + swLatlng.toString() + ',' + 
-                '북동쪽 위경도 : '+ neLatlng.toString());
-            })
+    useEffect(() => {
+        mapscript();
+
+        const script = document.createElement("script");
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAOMAP_API_KEY}&libraries=services,drawing`;
+        script.async = true;
+        script.onload = () => {
+            mapscript();
+        };
+        script.onerror = (error) => {
+            console.error("Error loading Kakao maps script:", error);
+        };
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, [mapscript]);
+
+    const closeOverlays = () => {
+        const overlays = document.querySelectorAll('.wrap');
+        overlays.forEach(overlay => overlay.style.display = 'none');
+        // 검색창 닫기
+        const searchBar = document.querySelector('.search-block');
+        if (searchBar) {
+            searchBar.style.display = 'none';
         }
     };
 
     return (
-        <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+        <div id="map" style={{ width: "1194px", height: "100vh" }}></div>
     );
 }
 
