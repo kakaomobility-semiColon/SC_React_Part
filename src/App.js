ac199d17dca831e812da0000e188ba8fb3b7c0cb
K0seoyoung <koseo12@hufs.ac.kr>
2025-02-05 00:21:43 +0900

    feat: kakao map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './Login';
 import Join from './Join';
+import Kakao from './components/Kakao';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/join" element={<Join />} />
         </Routes>
+        <Kakao />
         <Footer />
       </div>
     </Router>
